eebe52deeea2a9489d9afe0f1e3dc916a1df0c1d
Beren <bozek3@uic.edu>
2021-06-13 22:44:39 -0500

    nevt changed to 1000

--- src/lcio_muon_gun.py
+++ src/lcio_muon_gun.py
@@ -17,7 +17,7 @@ from array import array
 from pyLCIO import UTIL, EVENT, IMPL, IO, IOIMPL
 
 #---- number of events ----------------------
-nevt = 10000
+nevt = 1000
 
 outfile = "muonGun_gen.slcio"
 
